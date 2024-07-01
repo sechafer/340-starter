0b8122e9593293d6a448ccdecc6a1c0467014b86
Samuel Chacon <sechafer@hotmail.com>
2024-07-01 15:56:08 -0300

    week 3 fix  index line 9 erase console.log

--- utilities/index.js
+++ utilities/index.js
@@ -6,7 +6,7 @@ const Util = {}
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
-console.log(data.rows) //
+//console.log(data.rows) //
   let list = "<ul>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
