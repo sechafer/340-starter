c60b72a8c38a0138109584ca685d0b990adfb571
Samuel Chacon <sechafer@hotmail.com>
2024-06-30 21:10:39 -0300

    Firts part week3

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const router = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -24,10 +27,10 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index Route
-app.get("/",function(req, res){
-    res.render("index", {title:"Home"})
-})
-
+//app.get("/",function(req, res){res.render("index", {title:"Home"})})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
