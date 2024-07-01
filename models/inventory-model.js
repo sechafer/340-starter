c56e09f9dd5a3ccdd65f2e3eea7fc2cd57f82bf9
Samuel Chacon <sechafer@hotmail.com>
2024-07-01 15:53:19 -0300

    week 3 fix index.js

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -12,18 +12,18 @@ async function getClassifications(){
  * ************************** */
 async function getInventoryByClassificationId(classification_id) {
     try {
-      const data = await pool.query(
-        `SELECT * FROM public.inventory AS i 
-        JOIN public.classification AS c 
-        ON i.classification_id = c.classification_id 
-        WHERE i.classification_id = $1`,
-        [classification_id]
-      )
-      return data.rows
-    } catch (error) {
-      console.error("getclassificationsbyid error " + error)
+        const data = await pool.query(
+          `SELECT * FROM public.inventory AS i 
+          JOIN public.classification AS c 
+          ON i.classification_id = c.classification_id 
+          WHERE i.classification_id = $1`,
+          [classification_id]
+        )
+        return data.rows
+      } catch (error) {
+        console.error("getclassificationsbyid error " + error)
+      }
     }
-  }
 
 
 module.exports = {getClassifications, getInventoryByClassificationId};
