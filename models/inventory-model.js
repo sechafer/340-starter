a1f71b6e83b75529647dc43d8684428f319d2e2b
Samuel Chacon <sechafer@hotmail.com>
2024-07-26 13:28:06 -0300

    intento 1

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,21 +1,22 @@
-const pool = require("../database/")
+const pool = require("../database/");
 
 /* ***************************
  *  Get all classification data
  * ************************** */
 async function getClassifications(everything = false){
   if (everything) {
-    return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+    return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
   } else {
     return await pool.query(
-      `SELECT c.classification_id, c.classification_name 
-      FROM public.classification AS C 
-      JOIN public.inventory as I 
-      ON c.classification_id = i.classification_id 
-      WHERE classification_approved = true 
-      AND inv_approved = true
-      GROUP BY c.classification_id 
-      ORDER BY classification_name`)
+      `SELECT c.classification_id, c.classification_name
+      FROM public.classification AS c
+      JOIN public.inventory AS i
+      ON c.classification_id = i.classification_id
+      WHERE c.classification_approved = true
+      AND i.inv_approved = true
+      GROUP BY c.classification_id
+      ORDER BY c.classification_name`
+    );
   }
 }
 
@@ -25,16 +26,16 @@ async function getClassifications(everything = false){
 async function getInventoryByClassificationId(classification_id) {
   try {
     const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
+      `SELECT * FROM public.inventory AS i
+      JOIN public.classification AS c
+      ON i.classification_id = c.classification_id
       WHERE i.classification_id = $1
-      AND inv_approved = true`,
+      AND i.inv_approved = true`,
       [classification_id]
-    )
-    return data.rows
+    );
+    return data.rows;
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    console.error("getclassificationsbyid error " + error);
   }
 }
 
@@ -44,25 +45,26 @@ async function getInventoryByClassificationId(classification_id) {
 async function getDetailsOfCar(inv_id) {
   try {
     const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
+      `SELECT * FROM public.inventory AS i
       WHERE i.inv_id = $1`,
       [inv_id]
-    )
-    return data.rows[0]
+    );
+    return data.rows[0];
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    console.error("getclassificationsbyid error " + error);
   }
 }
+
 async function error500() {
   try {
     const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
+      `SELECT * FROM public.inventory AS i
       WHERE i.type = 8`
-    )
-    return data.rows[0]
+    );
+    return data.rows[0];
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
-    return "Server Error"
+    console.error("getclassificationsbyid error " + error);
+    return "Server Error";
   }
 }
 
@@ -72,13 +74,12 @@ async function error500() {
 async function addClassification(classification_name) {
   try {
     const data = await pool.query(
-
       `INSERT INTO public.classification (classification_name) VALUES ($1)`,
       [classification_name]
-    )
-    return "Adding classification was succesful"
+    );
+    return "Adding classification was successful";
   } catch (error) {
-    console.error("Addingclassification error " + error)
+    console.error("Adding classification error " + error);
   }
 }
 
@@ -87,11 +88,11 @@ async function addClassification(classification_name) {
  * ************************** */
 async function checkExistingClassification(classification_name){
   try {
-    const sql = "SELECT * FROM public.classification WHERE classification_name = $1"
-    const classification = await pool.query(sql, [classification_name])
-    return classification.rowCount
+    const sql = "SELECT * FROM public.classification WHERE classification_name = $1";
+    const classification = await pool.query(sql, [classification_name]);
+    return classification.rowCount;
   } catch (error) {
-    return error.message
+    return error.message;
   }
 }
 
@@ -101,14 +102,13 @@ async function checkExistingClassification(classification_name){
 async function addVehicle(classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) {
   try {
     const data = await pool.query(
-
-      `INSERT INTO public.inventory (classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) 
+      `INSERT INTO public.inventory (classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
       [classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color]
-    )
-    return "Adding classification was succesful"
+    );
+    return "Adding vehicle was successful";
   } catch (error) {
-    console.error("Addingclassification error " + error)
+    console.error("Adding vehicle error " + error);
   }
 }
 
@@ -117,15 +117,13 @@ async function addVehicle(classification_id, inv_make, inv_model, inv_descriptio
  * ************************** */
 async function UpdateVehicle(classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color, inv_id) {
   try {
-    const sql = 
-      "UPDATE public.inventory SET classification_id = $1, inv_make = $2, inv_model = $3, inv_description = $4, inv_image = $5, inv_thumbnail = $6, inv_price = $7, inv_year = $8, inv_miles = $9, inv_color = $10 WHERE inv_id = $11 RETURNING *"
+    const sql =
+      "UPDATE public.inventory SET classification_id = $1, inv_make = $2, inv_model = $3, inv_description = $4, inv_image = $5, inv_thumbnail = $6, inv_price = $7, inv_year = $8, inv_miles = $9, inv_color = $10 WHERE inv_id = $11 RETURNING *";
 
-    const data = await pool.query(sql, [classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color, inv_id
-
-    ])
-    return data.rows[0]
+    const data = await pool.query(sql, [classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color, inv_id]);
+    return data.rows[0];
   } catch (error) {
-    console.error("model error: " + error)
+    console.error("Update vehicle error: " + error);
   }
 }
 
@@ -134,52 +132,52 @@ async function UpdateVehicle(classification_id, inv_make, inv_model, inv_descrip
  * ************************** */
 async function deleteVehicle(inv_id) {
   try {
-    const sql = 'DELETE FROM public.inventory WHERE inv_id = $1'
-    const data = await pool.query(sql, [inv_id])
-    return data
+    const sql = 'DELETE FROM public.inventory WHERE inv_id = $1';
+    const data = await pool.query(sql, [inv_id]);
+    return data;
   } catch (error) {
-    new Error("Delete Inventory error")
+    throw new Error("Delete Inventory error");
   }
 }
 
 async function pendingInv(account) {
   try {
     if (account.account_type == "Admin") {
-      const data = await pool.query('SELECT * FROM public.inventory WHERE inv_approved = false')
-      return data
+      const data = await pool.query('SELECT * FROM public.inventory WHERE inv_approved = false');
+      return data;
     } else {
-      new Error("Please, login")
+      throw new Error("Please, login");
     }
   } catch (error) {
-    new Error("Approved - pending inventory error")
+    throw new Error("Approved - pending inventory error");
   }
 }
 
 async function pendingClass(account) {
   try {
     if (account.account_type == "Admin") {
-      const data = await pool.query('SELECT * FROM public.classification WHERE classification_approved = false')
-      return data
+      const data = await pool.query('SELECT * FROM public.classification WHERE classification_approved = false');
+      return data;
     } else {
-      new Error("Please, login")
+      throw new Error("Please, login");
     }
   } catch (error) {
-    new Error("Approved - pending classification error")
+    throw new Error("Approved - pending classification error");
   }
 }
 
 /* ***************************
- *  Get all information of cassification by classification_id
+ *  Get all information of classification by classification_id
  * ************************** */
 async function getDetailsOfClassification(classification_id) {
   try {
     const data = await pool.query(
       `SELECT * FROM public.classification AS c WHERE c.classification_id = $1`,
       [classification_id]
-    )
-    return data.rows[0]
+    );
+    return data.rows[0];
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    console.error("getclassificationsbyid error " + error);
   }
 }
 
@@ -188,15 +186,18 @@ async function getDetailsOfClassification(classification_id) {
  * ************************** */
 async function approvedClassification(classification_id, account_id) {
   try {
-    const sql = `UPDATE public.classification 
-      SET classification_approved = $2, 
-      account_id = $3,
-      classification_approval_date = CURRENT_TIMESTAMP 
-      WHERE classification_id = $1 RETURNING *`
-    const data = await pool.query(sql, [classification_id, true, account_id])
-    return data
+    const sql = `
+      UPDATE public.classification
+      SET classification_approved = $2,
+          account_id = $3,
+          classification_approval_date = CURRENT_TIMESTAMP
+      WHERE classification_id = $1
+      RETURNING *
+    `;
+    const data = await pool.query(sql, [classification_id, true, account_id]);
+    return data;
   } catch (error) {
-    new Error("Update classification error")
+    throw new Error("Update classification error");
   }
 }
 
@@ -205,27 +206,34 @@ async function approvedClassification(classification_id, account_id) {
  * ************************** */
 async function rejecClassification(classification_id) {
   try {
-    const sql = `DELETE FROM public.classification WHERE classification_id = $1 RETURNING *`
-    const data = await pool.query(sql, [classification_id])
-    return data
+    const sql = `
+      DELETE FROM public.classification
+      WHERE classification_id = $1
+      RETURNING *
+    `;
+    const data = await pool.query(sql, [classification_id]);
+    return data;
   } catch (error) {
-    new Error("Update classification error")
+    throw new Error("Update classification error");
   }
 }
 
 /* ***************************
- *  Get require vahicles to delete from Database - reject
+ *  Get require vehicles to delete from Database - reject
  * ************************** */
 async function buildVehiclesToReject(classification_id) {
   try {
-    const sql = `SELECT inv_make, inv_model FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`
-    const data = await pool.query(sql, [classification_id])
-    return data
+    const sql = `
+      SELECT inv_make, inv_model
+      FROM public.inventory AS i
+      JOIN public.classification AS c
+      ON i.classification_id = c.classification_id
+      WHERE i.classification_id = $1
+    `;
+    const data = await pool.query(sql, [classification_id]);
+    return data;
   } catch (error) {
-    new Error("Error getting require vehicles.")
+    throw new Error("Error getting require vehicles.");
   }
 }
 
@@ -234,20 +242,25 @@ async function buildVehiclesToReject(classification_id) {
  * ************************** */
 async function approvedInventory(classification_id, account_id, inv_id) {
   try {
-    const verify = await pool.query("SELECT * FROM public.classification WHERE classification_id = $1 AND classification_approved = $2", [classification_id, true])
+    const verify = await pool.query(
+      "SELECT * FROM public.classification WHERE classification_id = $1 AND classification_approved = $2",
+      [classification_id, true]
+    );
     if (verify.rowCount > 0) {
-      const sql = `UPDATE public.inventory 
-        SET inv_approved = $2, 
-        account_id = $3,
-        inv_approved_date = CURRENT_TIMESTAMP, 
-        classification_id = $4
-        WHERE inv_id = $1 RETURNING *`
-      const data = await pool.query(sql, [inv_id, true, account_id, classification_id])
-      return data
+      const sql = `
+        UPDATE public.inventory
+        SET inv_approved = $2,
+            account_id = $3,
+            inv_approved_date = CURRENT_TIMESTAMP,
+            classification_id = $4
+        WHERE inv_id = $1
+        RETURNING *
+      `;
+      const data = await pool.query(sql, [inv_id, true, account_id, classification_id]);
+      return data;
     }
-
   } catch (error) {
-    new Error("Update Inventory error")
+    throw new Error("Update Inventory error");
   }
 }
 
@@ -256,16 +269,34 @@ async function approvedInventory(classification_id, account_id, inv_id) {
  * ************************** */
 async function rejecInventory(inv_id) {
   try {
-    const sql = `DELETE FROM public.inventory WHERE inv_id = $1 RETURNING *`
-    const data = await pool.query(sql, [inv_id])
-    return data
+    const sql = `
+      DELETE FROM public.inventory
+      WHERE inv_id = $1
+      RETURNING *
+    `;
+    const data = await pool.query(sql, [inv_id]);
+    return data;
   } catch (error) {
-    new Error("Reject inventory error")
+    throw new Error("Reject inventory error");
   }
 }
 
-module.exports = {getClassifications, getInventoryByClassificationId, 
-  getDetailsOfCar, error500, addClassification, checkExistingClassification, 
-  addVehicle, UpdateVehicle, deleteVehicle, pendingInv, pendingClass, getDetailsOfClassification,
-  approvedClassification, rejecClassification, buildVehiclesToReject, approvedInventory,
-  rejecInventory}
+module.exports = {
+  getClassifications,
+  getInventoryByClassificationId,
+  getDetailsOfCar,
+  error500,
+  addClassification,
+  checkExistingClassification,
+  addVehicle,
+  UpdateVehicle,
+  deleteVehicle,
+  pendingInv,
+  pendingClass,
+  getDetailsOfClassification,
+  approvedClassification,
+  rejecClassification,
+  buildVehiclesToReject,
+  approvedInventory,
+  rejecInventory
+};
